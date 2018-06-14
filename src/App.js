0884866a76f0d56d81e61d5596d94b4972640341
Unknown <lingosalad@gmail.com>
2018-06-13 20:28:56 -0400

    Mock ajax request and recipe list model

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import List from "./List.js";
 import Recipe from "./Recipe.js";
+import Header from "./Header.js";
 
 const App = () => (
     <Router>
         <div>
             <div>
-            header goes here
+            <Header/>
             <Link to="/recipe">Recipe</Link>
             <Link to="/">back to list</Link>
             </div>
--- src/List.js
+++ src/List.js
@@ -2,12 +2,44 @@ import React, { Component } from "react";
 import "./List.css";
 
 class List extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: null,
+            isLoaded: false,
+            recipes: []
+        }
+    }
+
+    componentDidMount() {
+        var recipes = new Array();
+        recipes.push({"id": 1, "title": "howards cherry pie"})
+        this.setState({
+            isLoaded: true
+            , recipes: recipes
+        })
+    }
+
     render() {
-        return (
-            <div className="List">
-                <h1> recipe list </h1>
-            </div>
-        );
+        const {error, isLoaded, recipes} = this.state;
+
+        if (error) {
+            return <div>Error loading recipes: {error.message}</div>
+        }
+        else if (!isLoaded) {
+            return <div>Loading...</div>
+        }
+        else {
+            return (
+               <div classname="List">
+                {recipes.map(recipe => (
+                    <li key={recipe.id}>
+                        {recipe.title}
+                    </li>
+                ))}
+                </div>
+            )
+        }
     }
 }
 
